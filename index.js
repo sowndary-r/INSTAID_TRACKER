5f61fce95f6c7bceaa3817e6c95ff11172ae7976
SOWNDARYA R <107302244+sowndary-r@users.noreply.github.com>
2023-03-05 21:02:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+let instaid = document.getElementById('insta_id').value;
+let link=`https://instagram.com/${instaid}?igshid=NTE5MzUyOTU=`;
+let btn=document.getElementById('btn');
+btn.addEventListener('click',()=>{window.location.replace("http://www.w3schools.com");});
